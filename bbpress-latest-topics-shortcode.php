2934636d0a8452635ed625a76040331f9014947d
Calvin Roger Canas <calvinrogercanas@gmail.com>
2015-06-06 11:14:34 +0800

    Add [bbpootle-forum-index]

---
+++ bbpress-latest-topics-shortcode.php
@@ -0,0 +1,170 @@
+<?php 
+
+/*
+Plugin Name: BbPress Latest Topic Shortcode
+Plugin URI: calvincanas.com
+Description: Enhanced [bbp-forum-index]
+Author: Calvin Canas
+Author URI: http://calvincanas.com
+Version: 1.0
+*/
+
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+if( !class_exists( 'Bb_Pootle' ) ):
+
+class Bb_Pootle {
+
+	public $codes;
+
+	public function __construct() {
+
+		$this->setup_shortcodes();
+		$this->add_shortcodes();
+	}
+
+	/**
+	 * Setup the shortcodes along with their respective shortcode
+	 * This is scalable just put it as array key($tag) and value($function)
+	 * @return void
+	 */
+	private function setup_shortcodes() {
+
+		$this->codes = apply_filters( 'bbpootle_shortcodes', array(
+
+				/**
+				 * Forums
+				 */
+				'bbpootle-forum-index'		=>		array( $this, 'display_chosen_topics' ),			
+			)
+		);
+	}
+
+	/**
+	 * This will be the engine that will register shortcodes base on the $codes array 
+	 * keys and values
+	 */
+	private function add_shortcodes() {
+		foreach ($this->codes as $code => $function) {
+			add_shortcode( $code, $function );
+		}
+	}
+
+	/**
+	 * Unset some globals in the $bbp object that hold query related info
+	 * Will also restore $post
+	 * @return void
+	 */
+	public function unset_globals() {
+		$bbp = bbpress();
+
+		// Unset global queries
+		$bbp->forum_query  = new WP_Query();
+		$bbp->topic_query  = new WP_Query();
+		$bbp->reply_query  = new WP_Query();
+		$bbp->search_query = new WP_Query();
+
+		// Unset global ID's
+		$bbp->current_view_id      = 0;
+		$bbp->current_forum_id     = 0;
+		$bbp->current_topic_id     = 0;
+		$bbp->current_reply_id     = 0;
+		$bbp->current_topic_tag_id = 0;
+
+		// Reset the post data
+		wp_reset_postdata();
+	}
+
+	/**
+	 * This actually exist in bbPress and I don't know why they set this into
+	 * private method since it is useful for plugin extension
+	 */
+	public function start( $query_name = '' ) {
+
+		// Set query name
+		bbp_set_query_name( $query_name );
+
+		// Start output buffer
+		ob_start();
+	}
+
+	/**
+	 * This actually exist in bbPress and I don't know why they set this into
+	 * private method since it is useful for plugin extension
+	 */
+	public function end() {
+
+		// Unset globals
+		$this->unset_globals();
+
+		// Reset the query name
+		bbp_reset_query_name();
+
+		// Return and flush the output buffer
+		return ob_get_clean();
+	}
+
+	
+	/**
+	 * The function behind shortcode [bbpootle]
+	 * @param  array $attr     This will catch the param value we got from user
+	 * @param  string $content [description]
+	 * @return string
+	 */
+	public function display_chosen_topics( $attr, $content = '' ) {
+		
+		// Sanity check required info
+		if ( !empty( $content ) || ( empty( $attr['forum_id'] ) ) )
+			return $content;
+
+		$this->unset_globals();
+
+		global $forum;
+		$forum = $attr['forum_id'];
+
+		if ( ! bbp_is_forum_archive() ) {
+			add_filter( 'bbp_before_has_forums_parse_args', array( $this, 'display_forum' ) );
+		}
+
+		// Start output buffer
+		$this->start( 'bbp_forum_archive' );
+		
+
+		// Output template
+		bbp_get_template_part( 'content', 'archive-forum' );
+
+		// Return contents of output buffer
+		return $this->end();
+
+	}
+
+	/**
+	 * Put the right value to our query args
+	 * @param  array $args this will be the argument we will parse to query specific forum
+	 * @return array       enhanced argument
+	 */
+	function display_forum( $args ) {
+		global $forum ;
+		
+		
+		// split the string into pieces
+		$forums = explode(',', $forum);
+				
+		$args['post__in'] = $forums;
+		$args['post_parent'] = '';
+		$args['orderby'] = 'post__in';
+		return $args;
+	}	
+
+}
+
+//launch the engine
+return new Bb_Pootle();
+
+endif;
+
+
+
+
